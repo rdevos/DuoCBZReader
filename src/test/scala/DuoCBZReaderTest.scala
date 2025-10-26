1334b7e4ca583a8365fdec251f2e133a079d3d84
developer <paul@afront.be>
2025-10-26 11:01:38 +0100

    add a test, add missing library dependency

--- src/test/scala/DuoCBZReaderTest.scala
+++ src/test/scala/DuoCBZReaderTest.scala
@@ -11,4 +11,13 @@ class DuoCBZReaderTest extends AnyFlatSpec with Matchers {
       val exitCode = p.waitFor
       assert(1 === exitCode)
   }
+
+  "DuoCBZReader main" should "exit with code 2 on cbz file not present" in {
+    val pb = new ProcessBuilder("java", "-cp", System.getProperty("java.class.path"), "be.afront.reader.DuoCBZReader",
+      "800", "1600", "missing1.cbz", "missing2.cbz")
+    val p = pb.start
+    val exitCode = p.waitFor
+    assert(2 === exitCode)
+  }
+
 }
