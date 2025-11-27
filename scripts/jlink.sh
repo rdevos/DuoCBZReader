15ff076b69329eaa80d1632d6c9a07ec3a5046f7
developer <paul@afront.be>
2025-11-27 15:37:10 +0100

    use correct compress value in jlink when JDK is too old

--- scripts/jlink.sh
+++ scripts/jlink.sh
@@ -2,10 +2,19 @@
 
 rm -rf custom-jre
 
+JDK_MAJOR=$(jlink --version | cut -d '.' -f 1)
+
+# Avoid warnings but still support older JDKs
+
+if [ "$JDK_MAJOR" -ge 14 ]; then
+    COMPRESS="--compress=zip-6"
+else
+    COMPRESS="--compress=2"
+fi
+
 jlink --module-path $JAVA_HOME/jmods \
       --add-modules java.base,java.desktop,java.logging,jdk.unsupported  \
-      --output custom-jre \
-      --compress=zip-6
+      --output custom-jre $COMPRESS
 
 
 
